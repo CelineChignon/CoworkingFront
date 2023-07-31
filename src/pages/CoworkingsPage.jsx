7fc7c34351da340d529e7876db8450a5536fce65
Céline Chignon <celine-chignon@lapiscine.pro>
2023-07-31 15:19:26 +0200

    add fetch Delete

--- src/pages/CoworkingsPage.jsx
+++ src/pages/CoworkingsPage.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 
 const CoworkingsPage = () => {
 
-
     const [coworkings, setCoworkings] = useState([])
+    const [deleteCoworkingMessage, setDeleteCoworkingMessage] = useState(null);
 
     const fetchApiCoworkings = async () => {
 
@@ -11,14 +11,24 @@ const CoworkingsPage = () => {
         const repApiCoworkingsJson = await repApiCoworkings.json()
         setCoworkings(repApiCoworkingsJson.data)
     }
+    //Je passe la variable deleteCoworkingMessage dans la fonction useEffect, à chaque utilisation de cette variable par un utilisateur la liste des coworkings sera mise à jour
     useEffect(() => {
         fetchApiCoworkings();
-    }, [])
+    }, [deleteCoworkingMessage])
+
+    //Je rajoute la méthode Delete, car un appel vers une api est automatiquement un GET donc pour indiquer à React se que je souhaite faire je rajoute une méthode.
+    const handleDeleteCoworking = async (coworkingId) => {
+        const repDeleteCoworking = await fetch(`http://localhost:3010/api/coworkings/${coworkingId}`, { method: "DELETE" })
+        const repDeleteCoworkingJson = await repDeleteCoworking.json()
+        setDeleteCoworkingMessage(repDeleteCoworkingJson.message)
+    }
 
 
     return (
 
         <div>
+            <h1>Liste de nos Coworkings:</h1>
+            {deleteCoworkingMessage && <p>{deleteCoworkingMessage}</p>}
             {coworkings.map((coworking) => (
                 <div key={coworking.id}>
                     <h2>{coworking.name}</h2>
@@ -27,7 +37,9 @@ const CoworkingsPage = () => {
                         {coworking.address.street} -
                         {coworking.address.postcode}
                         {coworking.address.city}
+
                     </p>
+                    <button onClick={() => handleDeleteCoworking(coworking.id)}>Supprimer le Coworking</button>
                 </div>
             ))}
         </div>
